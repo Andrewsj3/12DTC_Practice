36def9ff7a536bdb1b557e3c04f589d0879a2a08
Andrewsj3 <andrewsj3@middleton.school.nz>
2022-03-08 19:05:00 +1300

    v2: Added more information so user knows how many seats are left

---
+++ 02_ticket_loop_v2.py
@@ -0,0 +1,27 @@
+def loop():
+    from collections import namedtuple
+    constants = namedtuple("consts", "MAX_TICKETS")
+    consts = constants(5)
+    count = 0
+    print(f"You have {consts.MAX_TICKETS} seats left.")
+    while True:
+        name = input("Please enter your name: ").title()
+        if name == 'Xxx':
+            break
+        else:
+            count += 1
+        if count == consts.MAX_TICKETS:
+            break
+        print(f"You have {consts.MAX_TICKETS - count} "
+              f"{'seats' if count < consts.MAX_TICKETS - 1 else 'seat'} left.")
+
+    if count == consts.MAX_TICKETS:
+        print("\nAll available tickets have been sold.")
+    else:
+        print(f"\nYou have sold {count} tickets.")
+        print(f"There are still {consts.MAX_TICKETS - count} " 
+              f"tickets available.")
+
+
+if __name__ == '__main__':
+    loop()

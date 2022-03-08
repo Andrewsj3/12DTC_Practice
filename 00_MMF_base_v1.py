c9515046aa1b5531740394f3da3f52115117ef7f
Andrewsj3 <andrewsj3@middleton.school.nz>
2022-03-08 13:48:48 +1300

    Create template for main program

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,23 @@
+# Import statements
+
+# Function definitions
+
+# Main routine
+
+# Initialize variables and data structures
+
+# Ask user if they want instructions on how to use the program
+
+# Start loop
+
+# Get name (can't be blank)
+
+# Get age (between 12 and 130)
+
+# Calculate ticket price
+
+# Ask for payment method (apply surcharge for credit)
+
+# Calculate total sales and profit
+
+# Output data to file (txt or csv)

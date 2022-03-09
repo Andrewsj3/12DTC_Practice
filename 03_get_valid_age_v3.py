0baaad7ed5e9a5b8bc6cce4ec148f85a7fe235f8
Andrewsj3 <andrewsj3@middleton.school.nz>
2022-03-09 15:21:13 +1300

    Improved text formatting so output is easier to read

--- 03_get_valid_age_v3.py
+++ 03_get_valid_age_v3.py
@@ -6,15 +6,15 @@ def get_age(prompt):
         try:
             age = int(input(prompt))
             if age < consts.MIN_AGE:
-                print("Sorry, you are too young for this movie.")
+                print("Sorry, you are too young for this movie.\n")
                 return 0
             elif age > consts.MAX_AGE:
-                print("Please enter an integer between 12 and 110.")
+                print("Please enter an integer between 12 and 110.\n")
             else:
                 return age
         except ValueError:
             print("Please enter an integer "
-                  "(i.e. a whole number with no decimals)")
+                  "(i.e. a whole number with no decimals)\n")
 
 
 if __name__ == '__main__':

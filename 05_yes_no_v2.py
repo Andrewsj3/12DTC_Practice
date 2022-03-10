cd1032d0a0f320419e777a7dd5a58aa9d1ae9215
Andrewsj3 <andrewsj3@middleton.school.nz>
2022-03-11 10:32:31 +1300

    Added question as a parameter and simplified checking the response

---
+++ 05_yes_no_v2.py
@@ -0,0 +1,17 @@
+def yes_no(question):
+    response = input(question).lower()
+    while True:
+        if response[0] == 'y':
+            return True
+        elif response[0] == 'n':
+            return False
+        else:
+            print("Invalid response, please enter Y or N")
+            response = input(question).lower()
+
+
+# if __name__ == '__main__':
+#     if yes_no("Do you want snacks? "):
+#         print("Valid answer, you do want snacks")
+#     else:
+#         print("Valid answer, you don't want snacks")

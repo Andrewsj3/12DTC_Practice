6abdf1b7b97f5bf95a73bc51938540aef91c96c5
Andrewsj3 <andrewsj3@middleton.school.nz>
2022-03-11 17:26:13 +1300

    v2: Added question and valid_choices as parameters

--- 06_string_validator_v2.py
+++ 06_string_validator_v2.py
@@ -9,12 +9,12 @@ def get_snacks(question, valid_choices):
         return get_snacks(question, valid_choices)
 
 
-if __name__ == '__main__':
-    ask_for_snack = "What snack do you want: "
-    valid_snacks = {('popcorn', 'p', 'corn', '1'): "Popcorn",
-                    ('m&ms', 'mms', 'm', '2'): "M&Ms",
-                    ('pita chips', 'chips', 'pc', 'pita', 'c', '3'):
-                        "Pita chips",
-                    ('w', 'water', '4'): "Water"}
-    for test in range(6):
-        print(f"You have chosen {get_snacks(ask_for_snack, valid_snacks)}")
+# if __name__ == '__main__':
+#     ask_for_snack = "What snack do you want: "
+#     valid_snacks = {('popcorn', 'p', 'corn', '1'): "Popcorn",
+#                     ('m&ms', 'mms', 'm', '2'): "M&Ms",
+#                     ('pita chips', 'chips', 'pc', 'pita', 'c', '3'):
+#                         "Pita chips",
+#                     ('w', 'water', '4'): "Water"}
+#     for test in range(6):
+#         print(f"You have chosen {get_snacks(ask_for_snack, valid_snacks)}")

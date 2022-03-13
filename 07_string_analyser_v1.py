f0bcc9310b3a6fdac4d7f465717de11769d18f7e
Andrewsj3 <andrewsj3@middleton.school.nz>
2022-03-14 08:54:02 +1300

    v1: Basic version of string analyser

---
+++ 07_string_analyser_v1.py
@@ -0,0 +1,8 @@
+def analyse_string():
+    my_string = "Hello World"
+    print(f"First letter: {my_string[0]}")
+    print(f"Rest of string: {my_string[1:]}")
+
+
+if __name__ == '__main__':
+    analyse_string()
